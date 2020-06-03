d95d4e2a7f05b0103550deab8f6058c573894425
unknown <rodgerthatxd@gmail.com>
2020-06-03 20:00:32 +1200

    First iternation of displaymanager

--- bird.vhd
+++ bird.vhd
@@ -8,12 +8,13 @@ ENTITY bird IS
 	PORT
 		(SIGNAL pb1, pb2, mouse1, clk, vert_sync	: IN std_logic;
         SIGNAL pixel_row, pixel_column		: IN std_logic_vector(9 DOWNTO 0);
-		SIGNAL red, green, blue 			: OUT std_logic);		
+		SIGNAL red, green, blue 			: OUT std_logic;		
+		signal bird_on : out std_logic);
 END bird;
 
 architecture behavior of bird is
 
-SIGNAL ball_on					: std_logic;
+SIGNAL bird_signal					: std_logic;
 SIGNAL size 					: std_logic_vector(9 DOWNTO 0);  
 SIGNAL ball_y_pos				: std_logic_vector(9 DOWNTO 0);
 SiGNAL ball_x_pos				: std_logic_vector(10 DOWNTO 0);
@@ -24,20 +25,29 @@ signal placeholderPB2 : std_logic := '0';
 BEGIN           
 
 size <= CONV_STD_LOGIC_VECTOR(8,10);
+
 -- ball_x_pos and ball_y_pos show the (x,y) for the centre of ball
 ball_x_pos <= CONV_STD_LOGIC_VECTOR(320,11);
 
-ball_on <= '1' when ( ('0' & ball_x_pos <= '0' & pixel_column + size) and ('0' & pixel_column <= '0' & ball_x_pos + size) 	-- x_pos - size <= pixel_column <= x_pos + size
+bird_signal <= '1' when ( ('0' & ball_x_pos <= '0' & pixel_column + size) and ('0' & pixel_column <= '0' & ball_x_pos + size) 	-- x_pos - size <= pixel_column <= x_pos + size
 					and ('0' & ball_y_pos <= pixel_row + size) and ('0' & pixel_row <= ball_y_pos + size) )  else	-- y_pos - size <= pixel_row <= y_pos + size
 			'0';
 
 
 
--- Colours for pixel data on video signal
--- Changing the background and ball colour by pushbuttons
-Red <=  placeholderPB1; -- pb1
-Green <= (not placeholderPB2) and (not ball_on); --pb2
-Blue <=  not ball_on;
+
+rgb:process (bird_signal)
+begin
+
+	bird_on <= bird_signal;
+
+	-- Colours for pixel data on video signal
+	-- Changing the background and ball colour by pushbuttons
+	Red <=  '1'; -- pb1
+	Green <= not bird_signal; --pb2
+	Blue <=  not bird_signal;
+	
+end process;
 
 
 Move_Bird: process (vert_sync, mouse1)  	
--- output_files/DisplayManager.vhd
+++ output_files/DisplayManager.vhd
@@ -5,5 +5,34 @@ use ieee.numeric_std.all;
 entity DisplayManager is 
 	port(
 	clk : in std_logic;
-	r, g, b : out std_logic_vector(2 downto 0));
+	bird_on : in std_logic;
+	r, g, b : out std_logic);
 end entity DisplayManager;
+
+architecture behaviour of DisplayManager is
+
+begin
+
+	pixel: process
+	
+	begin
+	
+	wait until clk'event and clk ='1';
+	
+	if bird_on = '1' then
+		
+		r <= '1';
+		g <= '0';
+		b <= '0';
+		
+	else
+		r <= '0';
+		g <= '1';
+		b <= '1';
+		
+	end if;
+
+		
+	end process pixel;
+	
+	end architecture;
