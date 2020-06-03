48f97948899a5ede6a44466a88dc1317f824f3f6
unknown <rodgerthatxd@gmail.com>
2020-06-04 04:26:16 +1200

    implemented pipes but doesnt work

--- output_files/DisplayManager.vhd
+++ output_files/DisplayManager.vhd
@@ -6,6 +6,7 @@ entity DisplayManager is
 	port(
 	clk : in std_logic;
 	bird_on : in std_logic;
+	pipe_on : in std_logic;
 	r, g, b : out std_logic);
 end entity DisplayManager;
 
@@ -20,9 +21,13 @@ begin
 	wait until clk'event and clk ='1';
 	
 	if bird_on = '1' then
-		
 		r <= '1';
 		g <= '0';
+		b <= '0';
+		
+	elsif pipe_on = '1' then
+		r <= '0';
+		g <= '1';
 		b <= '0';
 		
 	else
