b04baf812887be829627c1549cf8cf52ea12b648
jeva126 <jeva126@aucklanduni.ac.nz>
2020-06-04 00:14:29 +1200

    more bug fixes to pipes

--- pipes.vhd
+++ pipes.vhd
@@ -19,12 +19,12 @@ signal pipes_on 				: std_logic;
 
 
 SIGNAL y_pos_gen           : std_logic_vector(9 downto 0);
+SIGNAL y_pos_gen_two       : std_logic_vector(9 downto 0);
 
-
-SIGNAL gap_y_pos				: std_logic_vector(9 DOWNTO 0);
+SIGNAL gap_y_pos				: std_logic_vector(9 DOWNTO 0)  := CONV_STD_LOGIC_VECTOR(350,10);
 SiGNAL gap_x_pos				: std_logic_vector(10 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(639,11);
-SIGNAL gapOne_y_pos			: std_logic_vector(9 DOWNTO 0);
-SiGNAL gapOne_x_pos			: std_logic_vector(10 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(959,11);
+SIGNAL gapOne_y_pos			: std_logic_vector(9 DOWNTO 0)  := CONV_STD_LOGIC_VECTOR(300,10);
+SiGNAL gapOne_x_pos			: std_logic_vector(10 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(999,11);
 
 SiGNAL gap_x_motion			: std_logic_vector(10 DOWNTO 0);
 SIGNAL gap_size_x 			: std_logic_vector(10 DOWNTO 0);
@@ -34,13 +34,9 @@ SIGNAL gap_size_y  			: std_logic_vector(9 DOWNTO 0);
 -------------------- power up item
 
 signal powerUp_y_pos       : std_logic_vector(9 downto 0);
-signal powerUp_x_pos       : std_logic_vector(10 downto 0) := conv_STD_LOGIC_VECTOR(0,11);
+signal powerUp_x_pos       : std_logic_vector(10 downto 0);
 
---signal powerUp_sizeInner_x       : std_logic_vector(10 downto 0);
---signal powerUp_sizeInner_y       : std_logic_vector(9 downto 0);
---
---signal powerUp_sizeOuter_x       : std_logic_vector(10 downto 0);
---signal powerUp_sizeOuter_y       : std_logic_vector(9 downto 0);
+
 
 signal powerUp_on : std_logic;
 
@@ -58,6 +54,7 @@ signal powerUp_sizeThree_y : std_logic_vector(9 downto 0);
 signal difficulty_level : std_logic_vector(1 downto 0) := "00"; -- 00 = easy / 01 = med /  10 = hard
 signal pipes_passed : integer := 0;
 
+signal tenPipe_checkpoint : integer := 0;
 BEGIN   
 
 gap_x_motion <= CONV_STD_LOGIC_VECTOR(1,11) when difficulty_level = "00" else
@@ -85,23 +82,30 @@ begin
 	case random_num is
 		
 		when "010" => y_pos_gen <= CONV_STD_LOGIC_VECTOR(110, 10);
+						  y_pos_gen_two <= CONV_STD_LOGIC_VECTOR(230, 10);
 		when "001" => y_pos_gen <= CONV_STD_LOGIC_VECTOR(150, 10);
+						  y_pos_gen_two <= CONV_STD_LOGIC_VECTOR(400, 10);
 		when "110" => y_pos_gen <= CONV_STD_LOGIC_VECTOR(100, 10);
+						  y_pos_gen_two <= CONV_STD_LOGIC_VECTOR(320, 10);
 		when "100" => y_pos_gen <= CONV_STD_LOGIC_VECTOR(190, 10);
+						  y_pos_gen_two <= CONV_STD_LOGIC_VECTOR(260, 10);
 		when "111" => y_pos_gen <= CONV_STD_LOGIC_VECTOR(350, 10);
+						  y_pos_gen_two <= CONV_STD_LOGIC_VECTOR(130, 10);
 		when "011" => y_pos_gen <= CONV_STD_LOGIC_VECTOR(220, 10);
+						  y_pos_gen_two <= CONV_STD_LOGIC_VECTOR(264, 10);
 		when "101" => y_pos_gen <= CONV_STD_LOGIC_VECTOR(260, 10);
+						  y_pos_gen_two <= CONV_STD_LOGIC_VECTOR(289, 10);
 		when others => y_pos_gen <= CONV_STD_LOGIC_VECTOR(250,10);
-		
+						  y_pos_gen_two <= CONV_STD_LOGIC_VECTOR(440, 10);
 	end case;
 
 end process gapGen ;
 ------------------------------------------------------------
 
-powerUp_on <= '1' when (((('0' & powerUp_y_pos - powerUp_sizeThree_y <= '0' & pixel_row) and ('0' & pixel_row <= '0' & powerUp_y_pos )) and 
-								((powerUp_x_pos - powerUp_sizeTwo_x <= pixel_column) and (pixel_column <= '0' & powerUp_x_pos - powerUp_sizeOne_x))) or 
-							((('0' & powerUp_y_pos - powerUp_sizetwo_y <= '0' & pixel_row) and ('0' & pixel_row <= '0' & powerup_y_pos - powerUp_sizeOne_y)) and 
-							  (('0' & powerUp_x_pos - powerUp_sizeThree_x <= '0' & pixel_column) and ('0' & pixel_column <= '0' & powerUp_x_pos))))	
+powerUp_on <= '1' when (((('0' & (powerUp_y_pos - powerUp_sizeThree_y) <= '0' & pixel_row) and ('0' & pixel_row <= '0' & powerUp_y_pos )) and 
+								(('0' & (powerUp_x_pos - powerUp_sizeTwo_x) <= '0' & pixel_column) and ('0' & pixel_column <= '0' & (powerUp_x_pos - powerUp_sizeOne_x)))) or 
+							((('0' & powerUp_y_pos - powerUp_sizetwo_y <= '0' & pixel_row) and ('0' & pixel_row <= '0' & (powerup_y_pos - powerUp_sizeOne_y))) and 
+							  (('0' & (powerUp_x_pos - powerUp_sizeThree_x) <= '0' & pixel_column) and ('0' & pixel_column <= '0' & powerUp_x_pos))))	
 						else 
 							'0';
 
@@ -126,6 +130,32 @@ Red <= (not '0') and ( not pipes_on) and (powerUp_on);
 Green <= '1' and (not powerUp_on) ; --pb1
 Blue <= (not pipes_on) and (not powerUp_on) ; 
 
+powerUpMovement: process (vert_sync, powerUp_x_pos, gap_x_motion, pipes_passed)
+
+begin
+
+	if (rising_edge(vert_sync)) then
+	
+		if (pipes_passed = (tenPipe_checkpoint + 10)) then
+			powerUp_x_pos <= CONV_STD_LOGIC_VECTOR(900,11);
+			powerUp_y_pos <= y_pos_gen;
+		else
+			
+			if(powerUp_x_pos <= CONV_STD_LOGIC_VECTOR(1,11)) then
+				powerUp_x_pos <= CONV_STD_LOGIC_VECTOR(1,11);
+			else
+				powerUp_x_pos <= powerUp_x_pos - gap_x_motion;
+			end if;
+		end if;
+			
+	
+	end if;
+
+end process powerUpMovement;
+
+
+
+
 
 gapPipe: process (vert_sync, gap_x_pos, pipes_passed)
 begin
@@ -137,20 +167,24 @@ begin
 			gap_x_pos <= CONV_STD_LOGIC_VECTOR(639, 11) + gap_size_x;
 			gap_y_pos <= y_pos_gen;
 			
-			if pipes_passed = 10 and difficulty_level /= "10" then
-				powerUp_x_pos <= CONV_STD_LOGIC_VECTOR(650,11);
-				powerUp_y_pos <= y_pos_gen;
-				difficulty_level <= difficulty_level + "01";
-				pipes_passed <= 0;
-			else
-				pipes_passed <= (pipes_passed + 2);
+			if pipes_passed = (tenPipe_checkpoint + 10) then
+			
+					if difficulty_level /= "10" then
+							difficulty_level <= difficulty_level + "01";
+					end if;
+					
+				tenPipe_checkpoint <= pipes_passed;
+				
+				
 			end if;
+			
+			pipes_passed <= (pipes_passed + 2);
+			
 		else
 			gap_x_pos <= gap_x_pos - gap_x_motion;
-			powerUp_x_pos <= powerUp_x_pos - gap_x_motion;--
+			
 		end if;
 
-		
 	end if;
 end process gapPipe;
 
@@ -162,7 +196,7 @@ begin
 		-- Bounce off top or bottom of the screen
 		if ( gapOne_x_pos   <= CONV_STD_LOGIC_VECTOR(1,11) ) then
 			gapOne_x_pos <= CONV_STD_LOGIC_VECTOR(639, 11) + gap_size_x;
-			gapOne_y_pos <= y_pos_gen;
+			gapOne_y_pos <= y_pos_gen_two;
 					
 		else
 			gapOne_x_pos <= gapOne_x_pos - gap_x_motion;--
