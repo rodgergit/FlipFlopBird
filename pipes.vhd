47b5844a6cb93c0c1c9ccd6cd9fb7aa743c4f936
jeva126 <jeva126@aucklanduni.ac.nz>
2020-06-04 21:03:35 +1200

    return of the pipes

--- pipes.vhd
+++ pipes.vhd
@@ -9,7 +9,10 @@ ENTITY pipes IS
 		(SIGNAL clk, vert_sync								: IN std_logic;
        SIGNAL pixel_row, pixel_column					: IN std_logic_vector(9 DOWNTO 0);
 		 SIGNAL random_num 									: IN std_logic_vector(2 downto 0);
-		 SIGNAL red, green, blue, pipe_on, powerUp_on				: OUT std_logic);		
+		 signal state : in std_logic_vector(2 downto 0);
+		 SIGNAL red, green, blue, pipe_on				: OUT std_logic;
+		 signal pipe_passed : out std_logic);
+		 
 END pipes;
 
 architecture behavior of pipes is
@@ -38,42 +41,59 @@ signal powerUp_x_pos       : std_logic_vector(10 downto 0);
 
 
 
-signal t_powerUp_on : std_logic;
+--signal t_powerUp_on : std_logic;
 
 
-signal powerUp_sizeOne_x : std_logic_vector(10 downto 0); 
-signal powerUp_sizeTwo_x : std_logic_vector(10 downto 0);
-signal powerUp_sizeThree_x : std_logic_vector(10 downto 0);
+--signal powerUp_sizeOne_x : std_logic_vector(10 downto 0); 
+--signal powerUp_sizeTwo_x : std_logic_vector(10 downto 0);
+--signal powerUp_sizeThree_x : std_logic_vector(10 downto 0);
+--
+--signal powerUp_sizeOne_y : std_logic_vector(9 downto 0);
+--signal powerUp_sizeTwo_y : std_logic_vector(9 downto 0);
+--signal powerUp_sizeThree_y : std_logic_vector(9 downto 0);
 
-signal powerUp_sizeOne_y : std_logic_vector(9 downto 0);
-signal powerUp_sizeTwo_y : std_logic_vector(9 downto 0);
-signal powerUp_sizeThree_y : std_logic_vector(9 downto 0);
+signal pipeOne_passed : std_logic;
+signal pipeZero_passed : std_logic;
+
+signal pipeOne_AlreadyPassed_flag : std_logic := '0';
+signal pipeZero_AlreadyPassed_flag : std_logic := '0';
 
 
+--signal difficulty_level : std_logic_vector(1 downto 0) := "00"; -- 00 = easy / 01 = med /  10 = hard
 
-signal difficulty_level : std_logic_vector(1 downto 0) := "00"; -- 00 = easy / 01 = med /  10 = hard
-signal pipes_passed : integer := 0;
 
-signal tenPipe_checkpoint : integer := 0;
+--signal tenPipe_checkpoint : integer := 0;
 BEGIN   
 
-gap_x_motion <= CONV_STD_LOGIC_VECTOR(1,11) when difficulty_level = "00" else
-					 CONV_STD_LOGIC_VECTOR(2,11) when difficulty_level = "01" else
-					 CONV_STD_LOGIC_VECTOR(3,11) when difficulty_level = "10" else
-					 CONV_STD_LOGIC_VECTOR(1,11);
+--gap_x_motion <= CONV_STD_LOGIC_VECTOR(1,11) when difficulty_level = "00" else -- must change to state
+--					 CONV_STD_LOGIC_VECTOR(2,11) when difficulty_level = "01" else
+--					 CONV_STD_LOGIC_VECTOR(3,11) when difficulty_level = "10" else
+--					 CONV_STD_LOGIC_VECTOR(1,11);
+					 
+					 
+	gap_x_motion <= conv_STD_LOGIC_VECTOR(1,11);
+--gap_x_motion <= CONV_STD_LOGIC_VECTOR(0,11) when state = "000" else -- need input from control unit , state
+--					 CONV_STD_LOGIC_VECTOR(1,11) when state = "001" else
+--					 CONV_STD_LOGIC_VECTOR(2,11) when state = "010" else
+--					 CONV_STD_LOGIC_VECTOR(3,11) when state = "011" else
+--					 CONV_STD_LOGIC_VECTOR(0,11) when state = "100" else
+--					 CONV_STD_LOGIC_VECTOR(0,11) when state = "101" else
+--					 CONV_STD_LOGIC_VECTOR(0,11) when state = "111" else
+--					 CONV_STD_LOGIC_VECTOR(0,11);
+--					 
 
 
 gap_size_x <= CONV_STD_LOGIC_VECTOR(100, 11);
 gap_size_y <= CONV_STD_LOGIC_VECTOR(75, 10);
 
 
-powerUp_sizeOne_x <= CONV_STD_LOGIC_VECTOR(5, 11); 
-powerUp_sizeTwo_x <= CONV_STD_LOGIC_VECTOR(10, 11);
-powerUp_sizeThree_x <= CONV_STD_LOGIC_VECTOR(15, 11);
-
-powerUp_sizeOne_y <= CONV_STD_LOGIC_VECTOR(5, 10);
-powerUp_sizeTwo_y <= CONV_STD_LOGIC_VECTOR(10, 10);
-powerUp_sizeThree_y <= CONV_STD_LOGIC_VECTOR(15, 10);
+--powerUp_sizeOne_x <= CONV_STD_LOGIC_VECTOR(5, 11); 
+--powerUp_sizeTwo_x <= CONV_STD_LOGIC_VECTOR(10, 11);
+--powerUp_sizeThree_x <= CONV_STD_LOGIC_VECTOR(15, 11);
+--
+--powerUp_sizeOne_y <= CONV_STD_LOGIC_VECTOR(5, 10);
+--powerUp_sizeTwo_y <= CONV_STD_LOGIC_VECTOR(10, 10);
+--powerUp_sizeThree_y <= CONV_STD_LOGIC_VECTOR(15, 10);
 
 
 gapGen : process (random_num)
@@ -101,13 +121,13 @@ begin
 
 end process gapGen ;
 ------------------------------------------------------------
-powerUp_on <= t_powerUp_on;
-t_powerUp_on <= '1' when (((('0' & (powerUp_y_pos - powerUp_sizeThree_y) <= '0' & pixel_row) and ('0' & pixel_row <= '0' & powerUp_y_pos )) and 
-								(('0' & (powerUp_x_pos - powerUp_sizeTwo_x) <= '0' & pixel_column) and ('0' & pixel_column <= '0' & (powerUp_x_pos - powerUp_sizeOne_x)))) or 
-							((('0' & powerUp_y_pos - powerUp_sizetwo_y <= '0' & pixel_row) and ('0' & pixel_row <= '0' & (powerup_y_pos - powerUp_sizeOne_y))) and 
-							  (('0' & (powerUp_x_pos - powerUp_sizeThree_x) <= '0' & pixel_column) and ('0' & pixel_column <= '0' & powerUp_x_pos))))	
-						else 
-							'0';
+--powerUp_on <= t_powerUp_on;
+--t_powerUp_on <= '1' when (((('0' & (powerUp_y_pos - powerUp_sizeThree_y) <= '0' & pixel_row) and ('0' & pixel_row <= '0' & powerUp_y_pos )) and 
+--								(('0' & (powerUp_x_pos - powerUp_sizeTwo_x) <= '0' & pixel_column) and ('0' & pixel_column <= '0' & (powerUp_x_pos - powerUp_sizeOne_x)))) or 
+--							((('0' & powerUp_y_pos - powerUp_sizetwo_y <= '0' & pixel_row) and ('0' & pixel_row <= '0' & (powerup_y_pos - powerUp_sizeOne_y))) and 
+--							  (('0' & (powerUp_x_pos - powerUp_sizeThree_x) <= '0' & pixel_column) and ('0' & pixel_column <= '0' & powerUp_x_pos))))	
+--						else 
+--							'0';
 
 -------------------------------------------------------------
 
@@ -126,86 +146,93 @@ pipes_on <= '1' when ((('0' & gap_x_pos - gap_size_x <= '0' & pixel_column) and 
 -- Colours for pixel data on video signal
 -- Changing the background and ball colour by pushbuttons
 
-Red <= (not '0') and ( not pipes_on) and (t_powerUp_on); 
-Green <= '1' and (not t_powerUp_on) ; --pb1
-Blue <= (not pipes_on) and (not t_powerUp_on) ; 
+Red <= (not '0') and ( not pipes_on) ; --and (t_powerUp_on); 
+Green <= '1' ;-- and (not t_powerUp_on) ; --pb1
+Blue <= (not pipes_on); -- and (not t_powerUp_on) ; 
 
-powerUpMovement: process (vert_sync, powerUp_x_pos, gap_x_motion, pipes_passed)
+--powerUpMovement: process (vert_sync, powerUp_x_pos, gap_x_motion, pipes_passed)
+--
+--begin
+--
+--	if (rising_edge(vert_sync)) then
+--	
+----		if (pipes_passed = (tenPipe_checkpoint + 10)) then
+----			powerUp_x_pos <= CONV_STD_LOGIC_VECTOR(900,11);
+----			powerUp_y_pos <= y_pos_gen;
+----		else
+----			
+----			if(powerUp_x_pos <= CONV_STD_LOGIC_VECTOR(1,11)) then
+----				powerUp_x_pos <= CONV_STD_LOGIC_VECTOR(1,11);
+----			else
+----				powerUp_x_pos <= powerUp_x_pos - gap_x_motion;
+----			end if;
+----		end if;
+----			
+----	
+----	end if;
+--
+--	
+--
+--end process powerUpMovement;
 
-begin
-
-	if (rising_edge(vert_sync)) then
-	
-		if (pipes_passed = (tenPipe_checkpoint + 10)) then
-			powerUp_x_pos <= CONV_STD_LOGIC_VECTOR(900,11);
-			powerUp_y_pos <= y_pos_gen;
-		else
-			
-			if(powerUp_x_pos <= CONV_STD_LOGIC_VECTOR(1,11)) then
-				powerUp_x_pos <= CONV_STD_LOGIC_VECTOR(1,11);
-			else
-				powerUp_x_pos <= powerUp_x_pos - gap_x_motion;
-			end if;
-		end if;
-			
-	
-	end if;
-
-end process powerUpMovement;
 
 
 
 
-
-gapPipe: process (vert_sync, gap_x_pos, pipes_passed)
+gapPipeZero: process (vert_sync, gap_x_pos)
 begin
 	-- Move ball once every vertical sync
 	if (rising_edge(vert_sync)) then
 	
-		-- Reset pipe position once it reaches the end of the screen
-		if ( gap_x_pos <= CONV_STD_LOGIC_VECTOR(1,11) ) then
+		if ( gap_x_pos  <= CONV_STD_LOGIC_VECTOR(1,11) ) then
 			gap_x_pos <= CONV_STD_LOGIC_VECTOR(639, 11) + gap_size_x;
-			gap_y_pos <= y_pos_gen;
-			
-			if pipes_passed = (tenPipe_checkpoint + 10) then
-			
-					if difficulty_level /= "10" then
-							difficulty_level <= difficulty_level + "01";
-					end if;
-					
-				tenPipe_checkpoint <= pipes_passed;
-				
-				
-			end if;
-			
-			pipes_passed <= (pipes_passed + 2);
-			
+			gap_y_pos <= y_pos_gen_two;
+			pipeZero_AlreadyPassed_flag <= '0';
 		else
-			gap_x_pos <= gap_x_pos - gap_x_motion;
+			if ( gap_x_pos  <= CONV_STD_LOGIC_VECTOR(320,11) and pipeZero_AlreadyPassed_flag = '0'  ) then
+				pipeZero_passed <= '1';
+				pipeZero_AlreadyPassed_flag <= '1';
+			else
+				pipeZero_passed <= '0';
+			end if;
+
+			gap_x_pos <= gap_x_pos - gap_x_motion;--
+			
+			
 			
 		end if;
-
+		
 	end if;
-end process gapPipe;
+end process gapPipeZero;
 
-gapPipeOne: process (vert_sync, gapOne_x_pos, pipes_passed)  	
+gapPipeOne: process (vert_sync, gapOne_x_pos)  	
 begin
 	-- Move ball once every vertical sync
 	if (rising_edge(vert_sync)) then
 
-		-- Bounce off top or bottom of the screen
-		if ( gapOne_x_pos   <= CONV_STD_LOGIC_VECTOR(1,11) ) then
+		--reset posiition
+		if ( gapOne_x_pos  <= CONV_STD_LOGIC_VECTOR(1,11) ) then
 			gapOne_x_pos <= CONV_STD_LOGIC_VECTOR(639, 11) + gap_size_x;
 			gapOne_y_pos <= y_pos_gen_two;
-					
+			pipeOne_AlreadyPassed_flag <= '0';
 		else
+			if ( gapOne_x_pos  <= CONV_STD_LOGIC_VECTOR(320,11) and pipeZero_AlreadyPassed_flag = '0' ) then
+				pipeOne_passed <= '1';
+				pipeone_AlreadyPassed_flag <= '1';
+			else
+				pipeOne_passed <= '0';
+			end if;
+
 			gapOne_x_pos <= gapOne_x_pos - gap_x_motion;--
+			
+			
+			
 		end if;
 		
 	end if;
 end process gapPipeOne;
 
-
+pipe_passed <= pipeOne_passed or pipeZero_passed;
 
 
 END behavior;
