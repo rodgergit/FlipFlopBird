5969668b656d7ce466f273800331c73d4a573bb0
mavbelt <61758726+mavbelt@users.noreply.github.com>
2020-06-05 04:25:07 +1200

    updated lives_manager

--- lives_manager.vhd
+++ lives_manager.vhd
@@ -19,25 +19,45 @@ end entity lives_manager;
 
 architecture arch of lives_manager is
     signal lives: unsigned(1 downto 0);
-    signal bird_on_prev: std_logic;
+    signal invincible: std_logic;
 begin
-    process (clk, reset, state, lives)
+    collision_detection: process (clk, reset, state, lives)
     begin
         if (reset = '0' or state = "000") then
+            invincible <= '0';
             lives <= "11";
         elsif (rising_edge(clk)) then
             if (state = "001" or state = "010" or state = "011") then
-                if (ground_on = '1' and bird_on = '1' and bird_on_prev = '0') then
+                if (ground_on = '1' and bird_on = '1' and invincible <= '0') then
+                    invincible <= '1';
                     lives <= "00";
-                elsif (pipe_on = '1' and bird_on = '1' and bird_on_prev = '0') then
+                elsif (pipe_on = '1' and bird_on = '1' and invincible <= '0') then
+                    invincible <= '1';
                     lives <= lives - 1;
-                elsif (powerup_on = '1' and bird_on = '1' and bird_on_prev = '0') then
+                elsif (powerup_on = '1' and bird_on = '1' and invincible <= '0') then
+                    invincible <= '1';
                     lives <= lives + 1;
                 end if;
-                bird_on_prev <= bird_on;
             end if;
         end if;
 
         lives_out <= std_logic_vector(lives);
-    end process;
+    end process collision_detection;
+
+    invincibility: process (clk, reset)
+    begin
+        variable count: unsigned(26 downto 0);
+        variable clk_2s: std_logic := '0';
+    begin
+        if (reset = '0') then
+            count := (others => '0');
+        elsif (rising_edge(clk)) then
+            if (count = to_unsigned(74999999, 27)) then
+                count := (others => '0');
+                invincible <= '0';
+            else
+                count := count + 1;
+            end if;
+        end if;
+    end process invincibility;
 end architecture arch;
